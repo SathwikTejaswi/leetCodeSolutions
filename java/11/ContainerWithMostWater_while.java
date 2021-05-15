a771f894b3b12eea12668824c4c61694ff8ab1f1
Deborshi Goswami <33585773+deborshigoswami95@users.noreply.github.com>
2021-05-14 18:41:52 -0700

    Create ContainerWithMostWater_while.java

---
+++ java/11/ContainerWithMostWater_while.java
@@ -0,0 +1,33 @@
+/** 
+Runtime: 2 ms, faster than 97.65% of Java online submissions for Container With Most Water.
+Memory Usage: 52.8 MB, less than 28.29% of Java online submissions for Container With Most Water.
+*/
+
+class Solution {
+    public int maxArea(int[] height) {
+        int left = 0;
+        int right = height.length - 1;
+        int maxArea = -1;
+        int currentArea = -1;
+        
+        while (left < right) {
+            
+            if (height[left] < height[right]) {
+                currentArea = (right - left) * height[left];
+                left += 1;
+            }
+            else {
+                currentArea = (right - left) * height[right];
+                right -= 1;
+            }
+            
+            if (currentArea > maxArea)
+                maxArea=currentArea;
+        }
+        
+        return maxArea; 
+        
+    }
+}
+
+  
